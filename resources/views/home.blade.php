0037b0a7b38caba04b6c174f68002b19758bd010
Siarhei <t375336306588@gmail.com>
2026-02-21 14:20:29 +0300

    Readme

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <iframe
+        src="https://z.fvds.ru/widget"
+        width="500"
+        height="600"
+        frameborder="0" >
+    </iframe>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\TicketController;
 use App\Http\Controllers\Auth\LoginController;
 
+Route::get('/', function () {
+    return view("home");
+});
+
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
